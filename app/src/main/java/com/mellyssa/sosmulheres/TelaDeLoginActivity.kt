6e80d46846ba16825ec4203ccf882aaf4e41a4f4
mellyssalves <mellyssalves@alu.ufc.br>
2023-09-18 13:14:17 -0300

    Criação da tela de login

---
+++ app/src/main/java/com/mellyssa/sosmulheres/TelaDeLoginActivity.kt
@@ -0,0 +1,11 @@
+package com.mellyssa.sosmulheres
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class TelaDeLoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.tela_de_login)
+    }
+}

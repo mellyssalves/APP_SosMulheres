19f092d984f72bc83078a022706010be8dfd1bec
mellyssalves <mellyssalves@alu.ufc.br>
2023-11-20 00:52:47 -0300

    criação de action bar na tela de options

--- app/src/main/java/com/mellyssa/sosmulheres/TelaOptions.kt
+++ app/src/main/java/com/mellyssa/sosmulheres/TelaOptions.kt
@@ -4,20 +4,29 @@ import android.os.Bundle
 import android.view.View
 import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
+import com.mellyssa.sosmulheres.databinding.ActivityMainBinding
+import com.mellyssa.sosmulheres.databinding.ActivityTelaOptionsBinding
+import com.mellyssa.sosmulheres.databinding.TelaDeLoginBinding
 
 
 class TelaOptions : AppCompatActivity() {
-
+    private val binding by lazy{
+        ActivityTelaOptionsBinding.inflate(layoutInflater)
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_tela_options)
-
-        val botaoDenunciar: ImageView  = findViewById(R.id.imageDenunciar)
-
-        botaoDenunciar.setOnClickListener{
-            val intent = Intent(this, TelaDeLoginActivity::class.java)
-            startActivity(intent)
+        setContentView(binding.root)
+        inicializaToolbar()
         }
-
+    private fun inicializaToolbar(){
+        val toolbar = binding.toolbarOptions.toolbar
+        setSupportActionBar(toolbar)
+        supportActionBar?.apply{
+            title = "TESTE"
+            setDisplayHomeAsUpEnabled(true)
+        }
     }
+
+
+
 }

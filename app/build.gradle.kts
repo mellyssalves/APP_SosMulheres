7a0fbebfb2f2eea4ed83daf10ff6b083ae581264
kassia <kassialopes201@gmail.com>
2023-12-03 02:43:28 -0300

    update Tela de Login

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+    id("com.google.gms.google-services")
 }
 
 android {
@@ -49,4 +50,11 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    implementation("com.google.firebase:firebase-auth")
+    implementation("com.google.firebase:firebase-bom:32.0.0")
+    implementation( platform("com.google.firebase:firebase-bom:32.5.0"))
+    implementation("com.google.firebase:firebase-auth-ktx")
+    implementation("com.google.firebase:firebase-firestore:24.9.1")
+    implementation ("com.google.firebase:firebase-analytics:17.2.1")
+    implementation ("com.google.firebase:firebase-database:19.2.0")
 }
--- app/src/main/java/com/mellyssa/sosmulheres/TelaDeLoginActivity.kt
+++ app/src/main/java/com/mellyssa/sosmulheres/TelaDeLoginActivity.kt
@@ -4,7 +4,16 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.mellyssa.sosmulheres.databinding.TelaDeLoginBinding
+import android.util.Log
+import android.widget.Toast
+import com.google.firebase.Firebase
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.auth
+import com.mellyssa.sosmulheres.databinding.ActivityCadastroBinding
+
+
 class TelaDeLoginActivity : AppCompatActivity() {
+    private lateinit var auth: FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.tela_de_login)
@@ -12,10 +21,38 @@ class TelaDeLoginActivity : AppCompatActivity() {
         binding = TelaDeLoginBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.botaoLogin.setOnClickListener{
-            val botaoLogin = Intent(this, TelaOptions::class.java)
-            startActivity( botaoLogin)
+        binding.botaoLogin.setOnClickListener {
+            val email: String = binding.editTextTextEmailAddress2.text.toString()
+            val senha: String = binding.editTextTextPassword2.text.toString()
+
+            if (email.isNotEmpty() && senha.isNotEmpty()) {
+                login(email, senha)
+            } else {
+                Toast.makeText(
+                    this@TelaDeLoginActivity,
+                    "Prencha os campos corretamente",
+                    Toast.LENGTH_SHORT,
+                ).show()
+            }
         }
-        }
-    private lateinit var binding: TelaDeLoginBinding
     }
+
+
+    private lateinit var binding: TelaDeLoginBinding
+
+    private fun login(email: String, senha: String) {
+        auth.signInWithEmailAndPassword(email, senha).addOnCompleteListener {
+            if (it.isSuccessful) {
+                val intent = Intent(this, TelaOptions::class.java)
+                startActivity(intent)
+            } else {
+                Toast.makeText(baseContext, "email ou senha inválidos", Toast.LENGTH_SHORT,).show()
+            }
+        }
+    }
+
+
+    companion object{
+        private var TAG= "emailEsenha"
+    }
+}

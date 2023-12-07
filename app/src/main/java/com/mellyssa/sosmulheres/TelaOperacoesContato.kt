567320c3bd563f536850563f6a49720487108714
kassia <kassialopes201@gmail.com>
2023-12-07 04:45:42 -0300

    atualização CRUD

--- app/src/main/java/com/mellyssa/sosmulheres/TelaOperacoesContato.kt
+++ app/src/main/java/com/mellyssa/sosmulheres/TelaOperacoesContato.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
 import com.google.firebase.Firebase
+import com.google.firebase.firestore.SetOptions
 import com.google.firebase.firestore.firestore
 import com.mellyssa.sosmulheres.databinding.ActivityListaContatosBinding
 import com.mellyssa.sosmulheres.databinding.ActivityTelaOperacoesContatoBinding
@@ -27,6 +28,25 @@ class TelaOperacoesContato : AppCompatActivity() {
                     for (document in it){
                         db.collection("contatos").document(document.id).delete().addOnSuccessListener {
                             Toast.makeText(applicationContext, "Contato excluido", Toast.LENGTH_SHORT).show()
+                        }
+                    }
+                }
+            }
+        }
+
+        binding.buttonAtualizar.setOnClickListener {
+            if(binding.nome.text.isNotEmpty() && binding.phone.text.isNotEmpty()){
+                val contato = hashMapOf(
+                    "nome" to binding.nome.text.toString(),
+                    "telefone" to binding.phone.text.toString()
+                )
+
+                val query = db.collection("contatos").whereEqualTo("nome", binding.nome.text.toString())
+                    .get()
+                query.addOnSuccessListener {
+                    for (document in it){
+                        db.collection("contatos").document(document.id).set(contato, SetOptions.merge()).addOnSuccessListener {
+                            Toast.makeText(applicationContext, "Contato atualizado", Toast.LENGTH_SHORT).show()
                         }
                     }
                 }
